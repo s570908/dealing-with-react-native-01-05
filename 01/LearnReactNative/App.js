ab1972cee364211e6b0e675fbcbe2ae6a1514ce1
s570908 <songohyoung@gmail.com>
2021-12-15 20:43:51 +0900

    1 react와 reqct-native 버젼을 높혀서 android studio 와 simulator가 잘 interworking하게 만들었다.

--- 01/LearnReactNative/App.js
+++ 01/LearnReactNative/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
 
 const App = () => {
+  console.log('로그가 어디에 보이나요?');
   return (
     <SafeAreaView>
       <View>
-        <Text>Hello React!</Text>
+        <Text>Hello React! 나와봐요.</Text>
       </View>
     </SafeAreaView>
   );
