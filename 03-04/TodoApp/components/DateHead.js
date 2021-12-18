65152f6ccc3aeba751f624bde07dfda88d527a4e
s570908 <songohyoung@gmail.com>
2021-12-18 23:30:13 +0900

    3.1.3.2 useSafeAreaInsets로 StatusBar 높이 구하기

--- 03-04/TodoApp/components/DateHead.js
+++ 03-04/TodoApp/components/DateHead.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 function DateHead({date}) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  const formatted = `${year}년 ${month}월 ${day}일`;
+  const {top} = useSafeAreaInsets();
 
   return (
     <>
+      <View style={[styles.statusBarPlaceholder, {height: top}]} />
       <StatusBar backgroundColor="#26a69a" />
       <View style={styles.block}>
-        <Text style={styles.dateText}>{formatted}</Text>
+        <Text style={styles.dateText}>
+          {year}년 {month}월 {day}일
+        </Text>
       </View>
     </>
   );
 }
 
-// 색상 코드를 정할 때는 Material Color(https://material.io/resources/color)를 주로 참조했습니다.
 const styles = StyleSheet.create({
+  statusBarPlaceholder: {
+    backgroundColor: '#26a69a',
+  },
   block: {
     padding: 16,
     backgroundColor: '#26a69a',
   },
   dateText: {
     fontSize: 24,
-    color: 'black',
+    color: 'white',
   },
 });
 
