5f311247227432d388b0b8c63115efa62481f1fa
s570908 <songohyoung@gmail.com>
2021-12-18 23:48:11 +0900

    3.1.4 레이아웃 준비하기

--- 03-04/TodoApp/components/Empty.js
+++ 03-04/TodoApp/components/Empty.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 function Empty() {
   return (
     <View style={styles.block}>
-      <Image
-        source={require('./../assets/images/young_and_happy.png')}
-        style={styles.image}
-      />
-      <Text style={styles.description}>야호! 할 일이 없습니다.</Text>
+      <Text style={styles.description}>야호! 할일이 없습니다.</Text>
     </View>
   );
 }
@@ -18,11 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  image: {
-    width: 240,
-    height: 179,
-    marginBottom: 16,
   },
   description: {
     fontSize: 24,
