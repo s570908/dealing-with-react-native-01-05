339385dfd4759fea595094acbc783e00913a4450
s570908 <songohyoung@gmail.com>
2021-12-20 00:34:03 +0900

    4.2.3 TodoItem 컴포넌트 만들기

--- 03-04/TodoApp/components/TodoItem.js
+++ 03-04/TodoApp/components/TodoItem.js
@@ -1,64 +1,18 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  Alert,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import {View, Text, StyleSheet} from 'react-native';
 
-function TodoItem({id, text, done, onToggle, onRemove}) {
-  const remove = () => {
-    Alert.alert(
-      '삭제',
-      '정말로 삭제하시겠어요?',
-      [
-        {text: '취소', onPress: () => {}, style: 'cancel'},
-        {
-          text: '삭제',
-          onPress: () => {
-            onRemove(id);
-          },
-          style: 'destructive',
-        },
-      ],
-      {
-        cancelable: true,
-        onDismiss: () => {},
-      },
-    );
-  };
-
+function TodoItem({id, text, done}) {
   return (
     <View style={styles.item}>
-      <TouchableOpacity onPress={() => onToggle(id)}>
-        <View style={[styles.circle, done && styles.filled]}>
-          {done && (
-            <Image
-              source={require('../assets/icons/check_white/check_white.png')}
-            />
-          )}
-        </View>
-      </TouchableOpacity>
-      <Text style={[styles.text, done && styles.lineThrough]}>{text}</Text>
-      {done ? (
-        <TouchableOpacity onPress={remove}>
-          <Icon name="delete" size={32} color="red" />
-        </TouchableOpacity>
-      ) : (
-        <View style={styles.removePlaceholder} />
-      )}
+      <View style={styles.circle} />
+      <Text style={styles.text}>{text}</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   item: {
     flexDirection: 'row',
     padding: 16,
-    borderBottomColor: '#e0e0e0',
     alignItems: 'center',
   },
   circle: {
@@ -69,23 +23,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginRight: 16,
   },
-  filled: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#26a69a',
-  },
   text: {
     flex: 1,
     fontSize: 16,
     color: '#212121',
-  },
-  lineThrough: {
-    color: '#9e9e9e',
-    textDecorationLine: 'line-through',
-  },
-  removePlaceholder: {
-    width: 32,
-    height: 32,
   },
 });
 
