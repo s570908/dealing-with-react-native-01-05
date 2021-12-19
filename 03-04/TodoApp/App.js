5710c4a7ae7e3bafd9547f97b83d4694ca6bbec7
s570908 <songohyoung@gmail.com>
2021-12-20 00:27:01 +0900

    4장 할일 목록 만들기 II
    4.2 todos 상태 만들기 및 FlatList로 항목 화면에 나타내기
    
    4.2.1 todos 상태 만들기

--- 03-04/TodoApp/App.js
+++ 03-04/TodoApp/App.js
@@ -4,9 +4,16 @@ import DateHead from './components/DateHead';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
+import {useState} from 'react';
 
 function App() {
   const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
 
   return (
     <SafeAreaProvider>
