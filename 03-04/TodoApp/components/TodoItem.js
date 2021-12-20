8145b0910e5656990ca23f7eeb34f0bc6f485c75
s570908 <songohyoung@gmail.com>
2021-12-20 16:02:44 +0900

    4.5.2 항목 삭제 함수 만들기

--- 03-04/TodoApp/components/TodoItem.js
+++ 03-04/TodoApp/components/TodoItem.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity, } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
-function TodoItem({id, text, done, onToggle}) {
+function TodoItem({ id, text, done, onToggle, onRemove }) {
   return (
     <View style={styles.item}>
       <TouchableOpacity onPress={() => onToggle(id)}>
         <View style={[styles.circle, done && styles.filled]}>
           {done && (
             <Image
-              source={require('../assets/icons/check_white/check_white.png')}
+              source={require("../assets/icons/check_white/check_white.png")}
             />
           )}
         </View>
       </TouchableOpacity>
-      <Text style={[styles.text, done && styles.lineThrough]}>{text}</Text>
-      { done ? (
-        <Icon name='delete' size={32} color='red' />
+      <Text style={[styles.text, done && styles.lineThrough]}>
+        {text}
+      </Text>
+      {done ? (
+        <TouchableOpacity onPress={() => onRemove(id)}>
+          <Icon name="delete" size={32} color="red" />
+        </TouchableOpacity>
       ) : (
         <View style={styles.removePlaceholder} />
       )}
@@ -26,36 +30,36 @@ function TodoItem({id, text, done, onToggle}) {
 
 const styles = StyleSheet.create({
   item: {
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 16,
-    alignItems: 'center',
+    alignItems: "center",
   },
   circle: {
     width: 24,
     height: 24,
     borderRadius: 12,
-    borderColor: '#26a69a',
+    borderColor: "#26a69a",
     borderWidth: 1,
     marginRight: 16,
   },
   filled: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#26a69a',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#26a69a",
   },
   text: {
     flex: 1,
     fontSize: 16,
-    color: '#212121',
+    color: "#212121",
   },
   lineThrough: {
-    color: '#9e9e9e',
-    textDecorationLine: 'line-through',
+    color: "#9e9e9e",
+    textDecorationLine: "line-through",
   },
   removePlaceholder: {
     width: 32,
     height: 32,
-  }
+  },
 });
 
 export default TodoItem;
