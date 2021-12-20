e0109045cd3d971c7d636a13a92d024f7221ddd3
s570908 <songohyoung@gmail.com>
2021-12-20 15:18:17 +0900

    4.4 할일 완료 상태 토글하기

--- 03-04/TodoApp/components/TodoList.js
+++ 03-04/TodoApp/components/TodoList.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { FlatList, View, Text, StyleSheet } from 'react-native';
+import {FlatList, View, StyleSheet} from 'react-native';
 import TodoItem from './TodoItem';
 
-function TodoList({ todos }) {
-    return (
-        <FlatList
-            ItemSeparatorComponent={() => <View style={styles.separator} />}
-            style={styles.list}
-            data={todos}
-            renderItem={({ item, index }) => (
-                <TodoItem id={item.id} text={item.text} done={item.done} />
-            )}
-            keyExtractor={item => item.id.toString()}
+function TodoList({todos, onToggle}) {
+  return (
+    <FlatList
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+      style={styles.list}
+      data={todos}
+      renderItem={({item}) => (
+        <TodoItem
+          id={item.id}
+          text={item.text}
+          done={item.done}
+          onToggle={onToggle}
         />
-    );
+      )}
+      keyExtractor={item => item.id.toString()}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
