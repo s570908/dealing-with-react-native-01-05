c5061f6287fcb333784233fce33f445e229e29d3
s570908 <songohyoung@gmail.com>
2021-12-20 19:18:47 +0900

    5.2.3 라우트 파라미터

--- 05/5.2/screens/DetailScreen.js
+++ 05/5.2/screens/DetailScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-function DetailScreen() {
+function DetailScreen({route}) {
+  // route라는 Props도 받아옵니다. 이 Props는 객체 타입
+  // {
+  //   "key": "Detail-vgDx8-H-8e7oao6a3xJz7",
+  //   "name": "Detail", 
+  //   "params": {"id": 1}
+  // }
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={styles.block}>
+      <Text style={styles.text}>id: {route.params.id}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 48,
+  },
+});
 
 export default DetailScreen;
