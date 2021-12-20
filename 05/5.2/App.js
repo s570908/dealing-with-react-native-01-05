1bc83c9ec0dd577dd95f50ad4b6085a9813e957b
s570908 <songohyoung@gmail.com>
2021-12-20 20:36:27 +0900

    5.2.5.4 헤더 숨기기

--- 05/5.2/App.js
+++ 05/5.2/App.js
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailScreen from './screens/DetailScreen';
 import {Text, TouchableOpacity, View} from 'react-native';
+import HeaderlessScreen from './screens/HeaderlessScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -52,6 +53,13 @@ function App() {
                 <Text>Right</Text>
               </View>
             ),
+          }}
+        />
+        <Stack.Screen
+          name="Headerless"
+          component={HeaderlessScreen}
+          options={{
+            headerShown: false,
           }}
         />
       </Stack.Navigator>
