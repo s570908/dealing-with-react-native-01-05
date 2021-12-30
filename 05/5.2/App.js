8cd974327c7e4a07f63de865bb9a17936fffcc4e
s570908 <songohyoung@gmail.com>
2021-12-31 02:53:35 +0900

    alert와 console.log를 활용하는 방법과 Header의 left를 cusomizing하기

--- 05/5.2/App.js
+++ 05/5.2/App.js
@@ -3,12 +3,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailScreen from './screens/DetailScreen';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {Text, TouchableOpacity, View, Alert} from 'react-native';
 import HeaderlessScreen from './screens/HeaderlessScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const onPress = props => () => {
+    console.log('props: ', props);
+    Alert.alert('Left를 눌렀나요?', `props: ${JSON.stringify(props)}`);
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -32,11 +36,14 @@ function App() {
           component={DetailScreen}
           options={{
             headerBackVisible: false,
-            headerLeft: ({onPress}) => (
-              <TouchableOpacity onPress={onPress}>
-                <Text>Left</Text>
-              </TouchableOpacity>
-            ),
+            headerLeft: props => {
+              console.log(props);
+              return (
+                <TouchableOpacity onPress={onPress(props)}>
+                  <Text>Left</Text>
+                </TouchableOpacity>
+              );
+            },
             headerTitle: ({children}) => (
               <View>
                 <Text>{children}</Text>
